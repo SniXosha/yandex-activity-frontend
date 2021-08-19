bb17fb156c26f6094e7ebf92aa40844f683bab89
Artem <you@example.com>
2021-08-20 00:03:42 +0300

    Latest work, a lot of govnokod

--- src/pages/main/ActivitiesContainer.tsx
+++ src/pages/main/ActivitiesContainer.tsx
@@ -1,22 +1,53 @@
 import React, {ReactElement} from "react";
-import {Container, makeStyles} from "@material-ui/core";
+import {Container, makeStyles, WithStyles} from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
 
 export default function ActivitiesContainer(): ReactElement {
     const classes = useStyles();
     return <Container className={classes.content} maxWidth={false}>
-        <Activity styleClass={classes.circle} title="Картинг"/>
-        <Activity styleClass={classes.circle} title="Серфинг"/>
-        <Activity styleClass={classes.circle} title="Велосипед"/>
+        <ActivityColumn styleClass={classes.activityColumn}>
+            <Activity size={17} image="surf" imageSize={250} title="Мужик в контрсвете"/>
+            <Activity size={15} image="cook" imageSize={200} title="Смешные томаты"/>
+            <Activity size={13} image="lasers" imageSize={200} title="Школьники"/>
+        </ActivityColumn>
+        <ActivityColumn styleClass={classes.activityColumn}>
+            <Activity size={13} image="horse" imageSize={150} title="Конь ебать"/>
+            <Activity size={17} image="planets" imageSize={200} title="Бэд трип"/>
+            <Activity size={15} image="karting" imageSize={170} title="Врум врум"/>
+        </ActivityColumn>
+        <ActivityColumn styleClass={classes.activityColumn}>
+            <Activity size={17} image="quest" imageSize={150} title="Держим дверь"/>
+            <Activity size={15} image="tea" imageSize={150} title="Чифирнуть"/>
+            <Activity size={13} image="sauna" imageSize={150} title="Три телки"/>
+        </ActivityColumn>
+        <ActivityColumn styleClass={classes.activityColumn}>
+            <Activity size={13} image="games" imageSize={200} title="Смотрим на стол"/>
+            <Activity size={17} image="rock" imageSize={150} title="Смеемся над камнем"/>
+            <Activity size={15} image="stretching" imageSize={150} title="Шпагат"/>
+        </ActivityColumn>
+    </Container>
+}
+
+interface ActivityColumnProps {
+    children: ReactElement | ReactElement[],
+    styleClass: string
+}
+
+function ActivityColumn({children, styleClass}: ActivityColumnProps) {
+    return <Container className={styleClass}>
+        {children}
     </Container>
 }
 
 interface ActivityProps {
-    styleClass: string,
+    size: number,
     title: string,
+    image: string,
+    imageSize: number,
 }
 
-function Activity({title, styleClass}: ActivityProps): ReactElement {
-    return <div className={styleClass}>{title}</div>
+function Activity({title, size, image, imageSize}: ActivityProps): ReactElement {
+    return <StyledButton image={image} imageSize={imageSize} size={size} title={title}/>
 }
 
 const useStyles = makeStyles(theme => ({
@@ -24,18 +55,47 @@ const useStyles = makeStyles(theme => ({
         height: "100%",
         width: "100%",
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
-    circle: {
-        margin: theme.spacing(2),
-        width: "10rem",
-        height: "10rem",
-        lineHeight: "10rem",
-        borderRadius: "50%",
-        fontSize: "1rem",
-        color: "#fff",
-        textAlign: "center",
-        background: "#000"
+    activityColumn: {
+        width: "17vw",
+        margin: "1vw"
     }
 }));
+
+interface Styles {
+    size: number;
+    image: string;
+    imageSize: number;
+    [key: string]: any;
+}
+
+interface ButtonStyles extends WithStyles<typeof styles> {
+    title: string;
+    size: number;
+    image: string;
+    imageSize: number;
+}
+
+const styles = {
+    root: {
+        margin: "1vw auto",
+        width: (props: Styles) => props['size'] + "vw",
+        height: (props: Styles) => props['size'] + "vw",
+        lineHeight: (props: Styles) => props['size'] + "vw",
+        borderRadius: "50%",
+        fontSize: "1rem",
+        color: "#FFF",
+        border: "solid 1px",
+        backgroundImage: (props: Styles) => `url(/images/${props['image']}.png)`,
+        backgroundSize: (props: Styles) => `${props['imageSize']}%`,
+        backgroundPosition: 'center',
+        borderColor: "#000",
+        textShadow: "1px 1px #000000"
+},
+};
+
+const StyledButton = withStyles(styles)(({ classes, title, size, ...other }: ButtonStyles) => (
+    <div className={classes.root}>{title}</div>
+));
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,13 +1,60 @@
 import React, {ReactElement} from "react";
-import {Container, makeStyles} from "@material-ui/core";
+import {Button, Container, makeStyles, Slider, Typography, withStyles} from "@material-ui/core";
 import ActivitiesContainer from "pages/main/ActivitiesContainer";
-
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
 
 export default function MainPage(): ReactElement {
     const classes = useStyles();
     return <Container className={classes.content} maxWidth={false}>
+        <Container className={classes.topBar}>
+            <ChooseBar/>
+            <WeekendInfoBar/>
+        </Container>
         <ActivitiesContainer/>
     </Container>
+}
+
+function ChooseBar(): ReactElement {
+    const classes = useStyles();
+    return <Container className={classes.choose}>
+        <Typography variant="h2">Выбирай, как провести время</Typography>
+        <Filters/>
+    </Container>
+}
+
+function WeekendInfoBar() {
+    const classes = useStyles();
+    return <Container className={classes.weekendInfo}>
+        <Typography variant="h6">До выходных: 2 дня</Typography>
+        <Container className={classes.days}>
+            <WeatherInfo day="сб"/>
+            <WeatherInfo day="вс"/>
+        </Container>
+    </Container>
+}
+
+function WeatherInfo({day}: any) {
+    return <div>
+        <WbSunnyIcon/>
+        <Typography>{day}</Typography>
+    </div>
+}
+
+function Filters(): ReactElement {
+    const classes = useStyles();
+    return <Container className={classes.filters}>
+        <NamedSlider name="Уровень активности"/>
+        <NamedSlider name="Бюджет"/>
+        <Button>Больше фильтров</Button>
+    </Container>
+}
+
+function NamedSlider({name}: any): ReactElement {
+    const classes = useStyles();
+    return <div className={classes.slider}>
+        {name}
+        <IOSSlider aria-labelledby="continuous-slider"/>
+    </div>
 }
 
 const useStyles = makeStyles(theme => ({
@@ -18,4 +65,89 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
         alignItems: 'center',
     },
+    days: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    weekendInfo: {
+        width: "20%"
+    },
+    topBar: {
+        height: "100%",
+        width: "100%",
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    choose: {
+        textAlign: "left",
+        width: "80%"
+    },
+    filters: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'left',
+        paddingLeft: "0.5vw"
+    },
+    slider: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: "10vw",
+    }
 }));
+
+const iOSBoxShadow =
+    '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const IOSSlider = withStyles({
+    root: {
+        color: '#3880ff',
+        height: 2,
+        padding: '15px 0',
+        margin: "0.25vw",
+        width: "80%"
+    },
+    thumb: {
+        height: 28,
+        width: 28,
+        backgroundColor: '#fff',
+        boxShadow: iOSBoxShadow,
+        marginTop: -14,
+        marginLeft: -14,
+        '&:focus, &:hover, &$active': {
+            boxShadow: '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+            // Reset on touch devices, it doesn't add specificity
+            '@media (hover: none)': {
+                boxShadow: iOSBoxShadow,
+            },
+        },
+    },
+    active: {},
+    valueLabel: {
+        left: 'calc(-50% + 12px)',
+        top: -22,
+        '& *': {
+            background: 'transparent',
+            color: '#000',
+        },
+    },
+    track: {
+        height: 2,
+    },
+    rail: {
+        height: 2,
+        opacity: 0.5,
+        backgroundColor: '#bfbfbf',
+    },
+    mark: {
+        backgroundColor: '#bfbfbf',
+        height: 8,
+        width: 1,
+        marginTop: -3,
+    },
+    markActive: {
+        opacity: 1,
+        backgroundColor: 'currentColor',
+    },
+})(Slider);
